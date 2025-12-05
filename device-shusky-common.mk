63a5370e7b06e1a2e7cbe32d965e6c7f4247d0fb
leegarchat <leegarchat@gmail.com>
2025-12-05 05:44:23 +0300

    Fix add thermel files

--- device-shusky-common.mk
+++ device-shusky-common.mk
@@ -25,7 +25,32 @@ PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_charge_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json
 
 # Custom thermal configs
-$(call inherit-product-if-exists,$(LOCAL_PATH)/thermal/PartCustomThermalConfigs.mk)
+PRODUCT_COPY_FILES += \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_battery_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_battery_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_battery_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_battery_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_battery_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_battery_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_battery_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_battery_soft.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_hard_battery_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_hard_battery_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_hard_battery_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_hard_battery_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_hard_battery_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_hard_battery_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_hard_battery_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_hard_battery_soft.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_medium_battery_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_medium_battery_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_medium_battery_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_medium_battery_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_medium_battery_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_medium_battery_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_medium_battery_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_medium_battery_soft.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_off_battery_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_off_battery_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_off_battery_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_off_battery_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_off_battery_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_off_battery_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_off_battery_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_off_battery_soft.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_soft.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_soft_battery_hard_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_soft_battery_hard.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_soft_battery_medium_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_soft_battery_medium.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_soft_battery_off_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_soft_battery_off.json \
+    $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/CustomConfigs/thermal_info_config_soc_soft_battery_soft_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_soc_soft_battery_soft.json
+
 # Power HAL config
 ifeq (,$(TARGET_VENDOR_PERF_CONFIG_PATH))
 TARGET_VENDOR_PERF_CONFIG_PATH := device/google/shusky/perf
