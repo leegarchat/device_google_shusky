ad736675d1de305dce64335a10d198c84ffa1524
leegarchat <leegarchat@gmail.com>
2025-12-09 19:19:48 +0300

    MigrateBrunch from Los23.1

--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -1,11 +1,10 @@
 #
-# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
-# SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
-TARGET_DISABLE_EPPE := true
+# Inherit some common stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -13,10 +12,6 @@ DEVICE_CODENAME := shiba
 DEVICE_PATH := device/google/shusky
 VENDOR_PATH := vendor/google/shiba
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-$(call inherit-product, device/google/zuma/lineage_common.mk)
-$(call inherit-product, device/google/zuma/evolution_common.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-evolution.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
--- shiba/BoardConfigEvolution.mk
+++
@@ -1,6 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2024 The Evolution X Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
-include $(DEVICE_PATH)/BoardConfigEvolution.mk
